f41d59bd41ca16f36cf88c14ac563fa26827eec4
reynaj671224 <reynaj671224@users.noreply.github.com>
2018-04-07 21:28:44 -0600

    Rev 2.6 - Materialea codigo 2 caracteres - login solo admin, inge, almacen

--- Coset_Sistema_Produccion/Forma_Materiales.cs
+++ Coset_Sistema_Produccion/Forma_Materiales.cs
@@ -196,9 +196,9 @@ namespace Coset_Sistema_Produccion
 
         private void Asigna_nuevo_folio_material()
         {
-            int numero_folio = Convert.ToInt32(folio_disponible.Folio_materiales.Substring(1, folio_disponible.Folio_materiales.Length - 1));
+            int numero_folio = Convert.ToInt32(folio_disponible.Folio_materiales.Substring(2, folio_disponible.Folio_materiales.Length - 2));
             numero_folio++;
-            folio_disponible.Folio_materiales = folio_disponible.Folio_materiales.Substring(0, 1) + numero_folio.ToString();
+            folio_disponible.Folio_materiales = folio_disponible.Folio_materiales.Substring(0, 2) + numero_folio.ToString();
             string respuesta = class_folio_disponible.Actualiza_Control_folios_base_datos(folio_disponible);
             if (respuesta != "")
                 MessageBox.Show(folio_disponible.error);
