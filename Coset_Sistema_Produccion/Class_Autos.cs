6610b6a6cb4ace760c68abedf0ae3d85421091fc
reynaj671224 <reynaj671224@users.noreply.github.com>
2018-05-01 22:06:45 -0600

    Autos class

---
+++ Coset_Sistema_Produccion/Class_Autos.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Coset_Sistema_Produccion
+{
+    public class Class_Autos
+    {
+        public List<Auto> Adquiere_autos_disponibles_en_base_datos()
+        {
+            List<Auto> clientes_disponibles = new List<Auto>();
+            MySqlConnection connection = new MySqlConnection(Configura_Cadena_Conexion_MySQL_almacen_autos());
+            try
+            {
+                MySqlCommand mySqlCommand = new MySqlCommand(Commando_leer_autos_disponibles(), connection);
+                connection.Open();
+                MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+                while (mySqlDataReader.Read())
+                {
+                    clientes_disponibles.Add(new Auto()
+                    {
+                        Placas = mySqlDataReader["placas"].ToString(),
+                        Marca = mySqlDataReader["marca"].ToString(),
+                        Color = mySqlDataReader["color"].ToString(),
+                        Modelo = mySqlDataReader["modelo"].ToString(),
+                        Descripcion = mySqlDataReader["descripcion"].ToString(),
+                        Status = mySqlDataReader["status"].ToString()
+
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                clientes_disponibles.Add(new Auto()
+                { error = ex.Message.ToString() });
+            }
+            connection.Close();
+            return clientes_disponibles;
+        }
+
+        private string Guarda_datos_auto(Auto auto)
+        {
+            MySqlConnection connection = new MySqlConnection(Configura_Cadena_Conexion_MySQL_almacen_autos());
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(Configura_cadena_comando_insertar_auto_en_base_de_datos(auto), connection);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                connection.Close();
+                return ex.Message;
+            }
+
+            connection.Close();
+            return "NO errores";
+        }
+
+        private string Modifica_datos_auto()
+        {
+            MySqlConnection connection = new MySqlConnection(Configura_Cadena_Conexion_MySQL_almacen_autos());
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(Configura_cadena_comando_modificar_auto_en_base_de_datos(), connection);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                connection.Close();
+                return ex.Message;
+            }
+
+            connection.Close();
+            return "NO errores";
+        }
+
+        private string Configura_cadena_comando_modificar_auto_en_base_de_datos(Auto auto)
+        {
+            return "UPDATE autos set  marca='" + auto.Marca +
+               "',color='" + auto.Color +
+               "',modelo='" + auto.Modelo +
+               "',descripcion='" + auto.Descripcion +
+               "',status='" + auto.Status +
+               "' where placas='" + auto.Placas + "';";
+        }
+
+        private string Configura_cadena_comando_insertar_auto_en_base_de_datos(Auto auto)
+        {
+            return "INSERT INTO autos(placas, marca,color,modelo,descripcion,status) " +
+               "VALUES('" + auto.Placas + "','" + auto.Marca + "'," +
+               "'" + auto.Color + "','" + auto.Modelo + "'" +
+               ",'" + auto.Descripcion + "','" + auto.Status + "');";
+        }
+
+        private string Commando_leer_autos_disponibles()
+        {
+            return "SELECT * FROM autos ORDER BY ABS(placas);";
+        }
+
+        private string Configura_Cadena_Conexion_MySQL_almacen_autos()
+        {
+            return "Server=" + Coset_Sistema_Produccion.ip_addres_server + ";Database=almacen;Uid=root;Pwd=" + Coset_Sistema_Produccion.password_server + ";";
+        }
+    }
+    public class Auto
+    {
+        public string Placas = "";
+        public string Marca = "";
+        public string Modelo = "";
+        public string Color = "";
+        public string Descripcion = "";
+        public string Status = "";
+        public string error = "";
+    }
+}
